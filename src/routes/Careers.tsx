9543644e27305c5e58a8e66ce7e7fd8d2237afe2
Eric Chen <eric.chen0121@gmail.com>
2024-03-06 15:41:44 -0800

    add hash router

---
+++ src/routes/Careers.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Careers = () => {
+  return <></>
+}
+
+export default Careers
---
+++ src/routes/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <>
+      <div>what Home it</div>
+    </>
+  )
+}
+
+export default Home
