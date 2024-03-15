08e36c671485daf7a2c07ccc356cdbee139887d9
Eric Chen <eric.chen0121@gmail.com>
2024-03-14 17:38:07 -0700

    add in player images

--- src/routes/About.tsx
+++ src/routes/About.tsx
@@ -1,23 +1,26 @@
 import React from 'react'
+import usePickPlayerStore from '../store/usePickPlayerStore'
 
 const About = () => {
+  // scroll snapping with tailwind: www.youtube.com/watch?v=iVTjsc4B9-I
+  const { players, round, roundPick, setRoundPick, setPlayers } =
+    usePickPlayerStore()
+
   return (
     <div className='snap-x snap-mandatory h-screen w-screen flex overflow-y-hidden'>
-      <div className='snap-always snap-start bg-yellow-200 flex-shrink-0 h-screen w-screen flex items-center justify-center text-5xl'>
-        1
-      </div>
-      <div className='snap-always snap-start bg-blue-200 flex-shrink-0 h-screen w-screen flex items-center justify-center text-5xl'>
-        2
-      </div>
-      <div className='snap-always snap-start bg-green-200 flex-shrink-0 h-screen w-screen flex items-center justify-center text-5xl'>
-        3
-      </div>
-      <div className='snap-always snap-start bg-green-200 flex-shrink-0 h-screen w-screen flex items-center justify-center text-5xl'>
-        4
-      </div>
-      <div className='snap-always snap-start bg-green-200 flex-shrink-0 h-screen w-screen flex items-center justify-center text-5xl'>
-        5
-      </div>
+      {players.map((player) => (
+        <div className='snap-always snap-start bg-yellow-200 flex-shrink-0 h-screen w-screen flex items-center justify-center text-5xl'>
+          <div
+            className='items-center'
+            style={{ display: 'flex', flexDirection: 'column' }}
+          >
+            <div>{player.name}</div>
+            <img
+              src={`https://static.www.nfl.com/image/private/f_png,q_100,h_800,w_800,c_fill,g_face:center,f_auto/%7B%7Binstance%7D%7D/god-draft-headshots/2024/${player?.image?.nfl_id}`}
+            />
+          </div>
+        </div>
+      ))}
     </div>
   )
 }
