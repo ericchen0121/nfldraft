a0fb34571c1d7241629b69b5c81971ad31622ea3
Eric Chen <eric.chen0121@gmail.com>
2024-03-18 13:28:59 -0700

    temp scroll fix end

--- src/routes/Team.tsx
+++ src/routes/Team.tsx
@@ -24,7 +24,7 @@ const Team = () => {
       setTimeout(() => {
         window.scrollTo({ top: 0, left: 0, behavior: 'smooth' })
         setScrolledToFirst(false)
-      }, allMocksByTeam.length * 120)
+      }, allMocksByTeam.length * 125)
     }
   }, [scrolledToFirst, allMocksByTeam])
 
